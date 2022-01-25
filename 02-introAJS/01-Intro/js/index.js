d659c1f7a3d9a0ad6d97a15b50c8f659e0d30e24
David Pelaez <davidpeva@gmail.com>
2022-01-24 22:27:40 -0500

    intro a JS

---
+++ 02-introAJS/01-Intro/js/index.js
@@ -0,0 +1,18 @@
+// Elementos para mostrar la salid de datos
+
+// muestra la salida en consola
+console.log("Hola mundo desde un archivo externo")
+
+console.log(5 + 10)
+
+console.log(4 === "4")
+
+// muestra la salida en la pag
+document.write("hola mundo, desde un archivo externo")
+document.write(5 + 10)
+document.write(4 === "4")
+
+// muestra un popup 
+alert("hola mundo, desde un archivo externo")
+alert(5 + 10)
+alert(4 === "4")
