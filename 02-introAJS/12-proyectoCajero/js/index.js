dea18976069b0ccace99861daf5ea454aa4a6d88
David Pelaez Valderrama <davidpelaezvalderrama@MacBook-Air-de-David.local>
2022-02-18 21:30:33 -0500

    aca funciona el login

--- 02-introAJS/12-proyectoCajero/js/index.js
+++ 02-introAJS/12-proyectoCajero/js/index.js
@@ -1,34 +1,63 @@
+//este es el objeto de datos
 var cuentas = [
     { 
-        nombre: 'David', 
-        clave: '123' 
+        user: 'David', 
+        password: '123', 
     },
     { 
-        nombre: 'Camila', 
-        clave: '456' 
+        user: 'Camila', 
+        password: '456', 
     },
     { 
-        nombre: 'Laura', 
-        clave: '789' 
+        user: 'Laura', 
+        password: '789', 
     }
 ];
 
-
+//funcion a ejecutar en el onclick
 function login() {
+    //declaro variables de que el dato q se captura en el input sea el dato q esta en el objeto
     var user = document.getElementById('user').value;
     var password = document.getElementById('password').value;
-
-    if(user === "David" && password === "123"){
-
+    //esta variable es la que me ayudara a q en el segundo if entre o diga vericiar
+    let acceder = false;
+    //ciclo para recorrer el objeto
+    for(i = 0; i < cuentas.length; i++){
+        if(user === cuentas[i].user && password === cuentas[i].password){
+            //aca digo si los datos estan buenos o no porque asi no me recorre todo el tiempo el arreglo
+            acceder = true;
+            localStorage.setItem('user', user.value);
+            localStorage.setItem('password', password.value);
+        }
+    }
+    //con este if le doy acceso si los datos estan buenos
+    if(acceder == true){
+        //este envia a la otra pagina
         window.location = "cajero.html";
-
+    }
+    else{
+        //esto sale si los datos estan malos
+        alert("Verifica tus datos")
     }
 }
 
 
 
 
-
+/*function login() {
+    var user = document.getElementById('user').value;
+    var password = document.getElementById('password').value;
+    
+    for(i = 0; i <= cuentas.length; i++){
+        if(user === cuentas[i].user && password === cuentas[i].password){
+            alert("Ingreso Exitoso");
+            window.location = "cajero.html";
+            return false; //ME DEJA INGRESAR A DAVID SIN ALERT
+    }else{
+    alert("Verifique sus datos");
+    }
+    }
+}*/
 
 
 //LOG IN
